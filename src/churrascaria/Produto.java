f765902e2190d0d1fce54b3e427940d36d6f63f1
Gabriel Borges Leite <gabrielborgesleitebl10@gmail.com>
2026-06-20 11:38:45 -0300

    Add files via upload

---
+++ src/churrascaria/Produto.java
@@ -0,0 +1,22 @@
+package churrascaria;
+
+public class Produto {
+	String nomeProduto;
+	double preco;
+	int quantidade;
+	
+	
+	Produto(String nomeProduto,int quantidade, double preco){
+		this.nomeProduto=nomeProduto;
+		this.quantidade=quantidade;
+		this.preco=preco;
+	}
+	
+	
+	@Override
+	 public String toString() {
+		 return nomeProduto + " | " + quantidade + "g | R$ " + preco;
+	    }
+	
+	
+}
